6f7d1fcc64954cca5474368ff2a51b3e0b41d39c
zarbagmedmehdi <zarbagmedmehdi>
2020-12-24 03:52:30 +0100

    third commit

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -14,7 +14,7 @@ class QuestionController extends AbstractController
     public function index(): Response
     {
         return $this->json([
-            'message' => 'Welcome to your new controller',
+            'message' => 'Welcome to your new controller!',
         ]);
     }
 }
