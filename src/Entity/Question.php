8b7b0677948c2d33b4f0ce0679812cc43d9f4fd8
zarbagmedmehdi <zarbagmedmehdi>
2020-12-24 02:43:03 +0100

    First commit

---
+++ src/Entity/Question.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=QuestionRepository::class)
+ */
+class Question
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contenu;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+}
---
+++ tests/Controller/QuestionControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class QuestionControllerTest extends WebTestCase
+{
+    public function testShowPost()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/question');
+        $string='{"message":"Welcome to your new controller!"}';
+        echo $client->getResponse()->getContent();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals($string, $client->getResponse()->getContent());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+    }
+}
